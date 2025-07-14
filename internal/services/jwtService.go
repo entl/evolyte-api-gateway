37c6cf0419f70e2a88259992bd820a9d90a5f8e6
entl <maxvorobyov01@gmail.com>
2025-07-14 21:20:31 +0100

    feat: add extract claims func

--- internal/services/jwtService.go
+++ internal/services/jwtService.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"errors"
+
 	"github.com/golang-jwt/jwt/v5"
 )
 
@@ -27,4 +28,20 @@ func (s *JwtService) ValidateToken(tokenString string) (bool, error) {
 	}
 
 	return true, nil
+}
+
+func (s *JwtService) ExtractClaims(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return s.jwtSecret, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, errors.New("invalid token claims")
 }
